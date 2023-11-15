da44a1a28418916af438f09fcf4378793ee5983d
Camilo <svg.z32@gmail.com>
2023-11-15 09:47:37 -0500

    packages 90%

---
+++ app/home/calculators/[projectId]/transmittance-roofs/page.tsx
@@ -0,0 +1,141 @@
+'use client';
+import PageWrapper from '../../../../../layout/PageWrapper/PageWrapper';
+import Page from '../../../../../layout/Page/Page';
+import Card, { CardBody, CardFooter } from '../../../../../components/bootstrap/Card';
+import React, { useEffect, useRef, useState } from 'react';
+import Input from '../../../../../components/bootstrap/forms/Input';
+import { TransmittanceTable } from '../../../../../components/tables/TransmittanceTable';
+import { CustomEditor } from '../../../../../components/extras/CustomEditor';
+import {
+	ButtonTypes,
+	SaveProjectButton,
+} from '../../../../../components/buttons/SaveProjectButton';
+import { useParams } from 'next/navigation';
+import BackToCalculatorsBtn from '../../../../../components/buttons/BackToCalculatorsBtn';
+import FileUploader from '../../../../../components/extras/FileUploader';
+import { useProjects } from '../../../../../services/project/project.service';
+import { NextButton } from '../../../../../components/buttons/NextButton';
+import { RoutesListWithParams } from '../../../../../common/constants/default';
+import Button from '../../../../../components/bootstrap/Button';
+import { router } from 'next/client';
+import Icon from '../../../../../components/icon/Icon';
+
+const keyName = 'transmittance';
+const TransmittancePage = () => {
+	const params = useParams();
+	const editor = useRef(null);
+	const [wallName, setWallName] = useState('');
+
+	const [data, setData] = useState();
+	const [dataResult, setDataResult] = useState();
+	const [editorText, setEditorText] = useState('');
+
+	const projects = useProjects();
+	const [initialData, setInitialData] = useState<any>({});
+	useEffect(() => {
+		projects
+			.getProjectData({ project_id: params?.projectId as string, key: keyName })
+			.then((data: any) => {
+				if (data) {
+					setInitialData(data.payload);
+					setData(data?.payload?.data);
+					setDataResult(data?.payload?.dataResult);
+					setWallName(data?.payload?.wallName);
+					setEditorText(data?.payload?.editorText);
+				}
+			});
+	}, []);
+	return (
+		<PageWrapper>
+			<Page>
+				<Card>
+					<CardBody>
+						<h4 className='fw-bold'>Cálculo de transmitancia térmica de muros</h4>
+					</CardBody>
+				</Card>
+
+				<Card>
+					<CardBody>
+						<div className='row'>
+							<div className='flex-column col-md-2 col-sm-12 align-self-center'>
+								<span>Nombre de muro tipo 1</span>
+							</div>
+							<div className='col-md-6 col-sm-12'>
+								<Input
+									placeholder='Ingresa el nombre del muro'
+									onChange={(e: any) => setWallName(e.target.value)}
+									value={wallName}></Input>
+							</div>
+							RSI 0.13 RSE 0.04
+						</div>
+					</CardBody>
+				</Card>
+				<div className='row align-content-between justify-content-between px-2'>
+					<Card className='col me-2'>
+						<CardBody>
+							<TransmittanceTable
+								onData={(e: any) => {
+									setData(e);
+								}}
+								data={data}
+							/>
+						</CardBody>
+					</Card>
+					<Card className='col-md-3'>
+						<CardBody>
+							<h4>Documentación entregada</h4>
+							<span>Planos de Fachada con dimensiones de ventanas y generales</span>
+							<FileUploader
+								projectId={params?.projectId as string}
+								keyName={keyName}
+							/>
+						</CardBody>
+					</Card>
+				</div>
+				<Card>
+					<CardBody>
+						<CustomEditor
+							placeholder='Detalle muro tipo'
+							initialText={editorText}
+							setText={(e: string) => {
+								setEditorText(e);
+							}}
+						/>
+					</CardBody>
+				</Card>
+
+				<Card>
+					<CardFooter>
+						<SaveProjectButton
+							payload={{
+								project_id: params?.projectId || '',
+								payload: { wallName, data, dataResult, editorText },
+								key: keyName,
+							}}
+							type={ButtonTypes.projectData}
+						/>
+						<BackToCalculatorsBtn />
+
+						<Button
+							color='link'
+							className='mx-1 col-2 m-0 p-0'
+							onClick={() =>
+								router.push(RoutesListWithParams.packages(params?.projectId))
+							}>
+							<span className='text-start'>
+								<Icon icon='Backpack' />
+								Paquetes
+							</span>
+						</Button>
+						<NextButton
+							route={RoutesListWithParams.calculatorWindow(params?.projectId)}
+							text='Siguiente'
+						/>
+					</CardFooter>
+				</Card>
+			</Page>
+		</PageWrapper>
+	);
+};
+
+export default TransmittancePage;
---
+++ components/tables/TransmittanceRoofTable.tsx
@@ -0,0 +1,203 @@
+import Input from '../bootstrap/forms/Input';
+import FormGroup from '../bootstrap/forms/FormGroup';
+import Button from '../bootstrap/Button';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Calculator } from '../../services/calculation/calculator';
+import { to2Decimal } from '../../helpers/helpers';
+
+interface RowProps {
+	data: any;
+	onInputChange: Function;
+	onRemove: Function;
+}
+
+const Row = ({ data, onInputChange, onRemove }: RowProps) => {
+	return (
+		<tr>
+			<td className='p-2'>
+				<Input
+					type='text'
+					value={data.column1}
+					onChange={(e: any) => onInputChange('column1', e.target.value)}
+				/>
+			</td>
+			<td className='p-2'>
+				<Input
+					name='ownerName'
+					type={'number'}
+					className=' text-center'
+					value={data.column2}
+					onChange={(e: any) => onInputChange('column2', e.target.value)}
+				/>
+			</td>
+			<td className='p-2'>
+				<Input
+					type='text'
+					value={data.column3}
+					onChange={(e: any) => onInputChange('column3', e.target.value)}
+				/>
+			</td>
+			<td className='p-2'>
+				<Input
+					className='col text-center'
+					inputMode={'decimal'}
+					value={data.column4}
+					onChange={(e: any) => onInputChange('column4', e.target.value)}
+				/>
+			</td>
+			<td className='p-2'>
+				<Input
+					className=' text-center'
+					inputMode={'decimal'}
+					value={data.column5}
+					onChange={(e: any) => onInputChange('column5', e.target.value)}
+				/>
+			</td>
+			<td className='p-2'>
+				<FormGroup id='width-window'>
+					<div className='d-flex align-content-between'>
+						<Button color='storybook' onClick={(e) => onRemove(e)}>
+							-
+						</Button>
+					</div>
+				</FormGroup>
+			</td>
+		</tr>
+	);
+};
+
+interface Props {
+	onData?: Function;
+	data?: { rows: []; result: { surface2: number } };
+	onDataResult?: Function;
+	dataResult?: { totalSurface1: number; totalSurface2: number; totalThickness: number };
+}
+
+export const TransmittanceTable = ({ onData, data }: Props) => {
+	const [row, setRow] = useState<any | []>([
+		{ column1: '', column2: '', column3: '', column4: '', column5: '' },
+	]);
+	const [totalThickness, setTotalThickness] = useState(0);
+	const [totalSurface1, setTotalSurface1] = useState(0);
+	const [totalSurface2, setTotalSurface2] = useState(0);
+
+	useEffect(() => {}, [totalSurface2]);
+	useEffect(() => {
+		if (data?.rows) setRow(data?.rows);
+		if (data?.result) setTotalSurface2(data?.result.surface2);
+	}, [data]);
+	const handleInputChange = (index: number, column: string | number, val: any) => {
+		const newRows = [...row];
+		if (column == 'column2' || column == 'column4' || column == 'column5')
+			newRows[index][column] = to2Decimal(val);
+		else newRows[index][column] = val;
+		const result = Calculator.calculateThickness(newRows, 'column5');
+
+		setTotalThickness(result);
+		setRow(newRows);
+	};
+
+	const calculateExpensive = useMemo(() => {
+		const surface1 = 100 - totalSurface2;
+		setTotalSurface1(surface1);
+		setTotalThickness(Calculator.calculateThickness(row, 'column5'));
+		const result = Calculator.transmittanceUValue(row, surface1, totalSurface2);
+		if (onData) onData({ rows: row, result: { surface2: totalSurface2, u_value: result } });
+		return result;
+	}, [row, totalSurface2]);
+
+	function handleDelete(id: number) {
+		let result: any = row.map((value: any, index: number) => {
+			if (index != id) return value;
+		});
+		result = result.filter((values: any) => values);
+		setRow(result);
+	}
+
+	function addRow() {
+		setRow([...row, { column1: '', column2: '', column3: '', column4: '', column5: '' }]);
+	}
+
+	return (
+		<>
+			<div className='m-2 my-4'>
+				<Button color='primary' onClick={addRow}>
+					Agregar fila
+				</Button>
+			</div>
+			<table>
+				<thead>
+					<tr className='text-center'>
+						<th className='px-2'>Superficie parcial 1</th>
+						<th className='px-2'>Valor λ(W/mk)</th>
+						<th className='px-2'>Superficie parcial 2</th>
+						<th className='px-2 '>Valor λ(W/mk)</th>
+						<th className='px-2 '>Espesor(m)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{row.map((value: any, index: number) => (
+						<Row
+							key={index}
+							data={value}
+							onRemove={() => handleDelete(index)}
+							onInputChange={(column: string | number, val: any) =>
+								handleInputChange(index, column, val)
+							}></Row>
+					))}
+					<tr>
+						<td className='p-2'>Porcentaje de superficie parcial</td>
+						<td className='p-2 align-self-center text-center'>
+							<div className='w-100 bg-info-subtle h4 bold  px-3  py-2 rounded'>
+								<span>{totalSurface1} %</span>
+							</div>
+						</td>
+						<td className='p-2 align-self-center'>
+							Porcentaje de superficie parcial 2
+						</td>
+						<td className='p-2 text-center   text-center'>
+							<FormGroup>
+								<div className='d-flex align-content-between'>
+									<Input
+										value={totalSurface2}
+										placeholder='%'
+										className='me-2 text-center '
+										inputMode={'decimal'}
+										onChange={(e: any) => {
+											if (
+												e.target.value.length <= 10 ||
+												e.target.value == 100
+											) {
+												setTotalSurface2(e.target.value);
+											}
+										}}
+									/>
+									<span className='align-self-center'>%</span>
+								</div>
+							</FormGroup>
+						</td>
+						<td className='p-2 align-self-center text-center'>
+							<div className='w-100 bg-info-subtle h4 bold  px-3 py-2 rounded'>
+								<span>{totalThickness} m</span>
+							</div>
+						</td>
+					</tr>
+
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td className='text-center'>
+							<span>Valor u</span>
+						</td>
+						<td className='p-2  text-center'>
+							<div className='w-100 bg-info-subtle h4 bold  px-3  py-2 rounded'>
+								<span className=''>{calculateExpensive} W/m²K</span>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</>
+	);
+};
